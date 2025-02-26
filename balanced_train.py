24b46c5bbe7726091c0e9db6cc0b27fe4e9a4b32
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2025-02-25 22:46:51 -0600

    added balanced versions of files

---
+++ balanced_train.py
@@ -0,0 +1,71 @@
+from ultralytics import YOLO
+import torch
+
+"""
+To disable sleep:
+sudo pmset -b sleep 0
+sudo pmset -b disablesleep 1
+
+To enable sleep:
+sudo pmset -b sleep 5
+sudo pmset -b disablesleep 0
+
+To purge RAM memory:
+sudo purge
+
+To disable CPU throttling:
+sudo pmset -a lidwake 0
+sudo pmset -a disablesleep 1
+
+To keep the macbook awake:
+caffeinate -i -d -m -u -t VALUE
+
+To get GPU usage:
+sudo powermetrics --samplers gpu_power
+
+To get memory usage:
+vm_stat
+
+To increase open file limit:
+ulimit -n 100000
+"""
+
+model = YOLO("yolo11n-p2.yaml")
+
+def clear_cache(_):
+    torch.mps.empty_cache()
+
+model.add_callback("on_train_batch_start", clear_cache)
+model.add_callback("on_val_batch_start", clear_cache)
+
+
+results = model.train(
+    # Train Variables
+    data="balanced_mapillary.yaml",
+    project="train",
+    name="balanced_dataset",
+    epochs=25,
+    val=False,
+    device="mps",
+    patience=3,
+    batch=32,
+    save_period=1,
+    imgsz=640,
+    exist_ok=True,
+    optimizer="AdamW",
+    amp=True, # mixed precision training
+    plots=True,
+    max_det=73, # The max number of annotations for an image is 73
+    show_boxes=True,
+    cos_lr=True, # Learning rate oscillates for better convergence
+    save_json=True,
+    augment=True,
+    seed=16,
+
+    # Augmentation Variables
+    mixup=0.5,
+    copy_paste=0.5,
+    copy_paste_mode="mixup",
+    degrees=22.5,
+    shear=5,
+)
