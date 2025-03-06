876733b8cd3f32ed5953e147ad5e455c016be9b8
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2025-03-06 10:57:17 -0600

    primarily changed box and dfl loss

--- balanced_dataset/rare_train.py
+++ balanced_dataset/rare_train.py
@@ -4,7 +4,7 @@ import torch
 pose_model = YOLO("yolo11n-pose.pt")
 
 # Converting pose model to detect
-detect_rare_model = YOLO("train/rare_balanced_augmented_640-4/weights/best.pt").load(pose_model.model)
+detect_rare_model = YOLO("train/detect_rare_model/weights/best.pt").load(pose_model.model)
 detect_rare_model.ckpt = {"model": detect_rare_model.model}
 detect_rare_model.save("models/detect_rare_model.pt")
 
@@ -20,15 +20,17 @@ detect_rare_model.train(
     data="balanced_dataset/small_object.yaml",
     project="train",
     name=f"detect_rare_model",
-    epochs=20,
+    epochs=16,
     device="mps",
     patience=15,
-    batch=40,
+    batch=64,
+    workers=12, # 12 CPU cores = 24 available threads
     save_period=1,
     imgsz=640,
     exist_ok=True,
     optimizer="AdamW",
     amp=True, # mixed precision training
+    rect=True,
     plots=True,
     max_det=73, # The max number of annotations for an image is 73
     cos_lr=True, # Learning rate oscillates for better convergence
@@ -36,13 +38,15 @@ detect_rare_model.train(
     seed=16,
     conf=0.01,
     iou=0.7,
-    rect=True,
     lr0=0.001,
     save_conf=True,
     save_crop=True,
     show_boxes=True,
     warmup_epochs=2, 
     close_mosaic=2,
+    # To give more weight to the keypoint loss
+    box=0.01,
+    dfl=0.01,
 
     # Augmentation Variables
     # I'm disabling these following parameters as we already did image augmentation
