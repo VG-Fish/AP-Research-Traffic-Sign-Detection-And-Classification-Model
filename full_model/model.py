0146cebe4c890bb5aceb2a0c960826a475213cb8
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2025-03-18 09:53:16 -0500

    created test dataset

--- full_model/model.py
+++ full_model/model.py
@@ -2,5 +2,14 @@ from ultralytics import YOLO
 
 model = YOLO("train/rare_balanced_augmented_640-4/weights/best.pt")
 
-metrics = model.val()
-print(metrics)
+metrics = model.val(
+    batch=48,
+    max_det=73,
+    plots=True,
+    save_json=True,
+    name="base_model",
+    project="train",
+    save_crop=True,
+    exist_ok=True,
+    device="mps",
+)
